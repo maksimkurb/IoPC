bdd1034cfe13eef10fa4ee13e8c5ef35fbe21d2a
Maxim Kurbatov <max@cubly.ru>
2021-04-13 20:52:49 +0400

    Improve web configurer

--- iopc-server/src/main/java/ru/cubly/iopc/module/websocket/WebSocketModule.java
+++ iopc-server/src/main/java/ru/cubly/iopc/module/websocket/WebSocketModule.java
@@ -13,23 +13,28 @@ import org.springframework.stereotype.Service;
 import ru.cubly.iopc.AbstractModule;
 import ru.cubly.iopc.action.Intent;
 import ru.cubly.iopc.module.CallableModule;
+import ru.cubly.iopc.module.ConfigurableModule;
 import ru.cubly.iopc.util.ModuleUtil;
 import ru.cubly.iopc.util.PlatformType;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static ru.cubly.iopc.util.Constants.HEADER_SERVICE_NAME;
 
 @Service
 @Slf4j
-public class WebSocketModule extends AbstractModule {
+public class WebSocketModule extends AbstractModule implements ConfigurableModule<WebSocketProperties> {
     private final List<CallableModule> modules;
+    private final WebSocketProperties webSocketProperties;
 
-    protected WebSocketModule(List<CallableModule> modules) {
+    protected WebSocketModule(List<CallableModule> modules, WebSocketProperties webSocketProperties) {
         super("websocket", Arrays.asList(PlatformType.Windows, PlatformType.Linux, PlatformType.MacOS));
 
         this.modules = modules;
+        this.webSocketProperties = webSocketProperties;
     }
 
     @Bean
@@ -54,5 +59,21 @@ public class WebSocketModule extends AbstractModule {
                 .transform(Intent::getPayload)
                 .route(router)
                 .get();
+    }
+
+    @Override
+    public WebSocketProperties getConfigFragmentModel() {
+        return webSocketProperties;
+    }
+
+    @Override
+    public Map<String, String> buildConfigProperties(WebSocketProperties model) {
+        HashMap<String, String> properties = new HashMap<>();
+
+        properties.put("websocket.secured", model.getSecured().toString());
+        properties.put("websocket.username", model.getUsername());
+        properties.put("websocket.password", model.getPassword());
+
+        return properties;
     }
 }
